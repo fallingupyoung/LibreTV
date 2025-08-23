dd71707403ce673afc194056cc8277fa497a8a84
fallingupyoung <83441827+fallingupyoung@users.noreply.github.com>
2025-08-24 02:29:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-@@ -1,214 +1,123 @@
+@@ -1,3 +1,4 @@
 // 全局常量配置
 const PROXY_URL = '/proxy/';    // 适用于 Cloudflare, Netlify (带重写), Vercel (带重写)
 // const HOPLAYER_URL = 'https://hoplayer.com/index.html';
@@ -114,11 +114,6 @@ const API_SITES = {
     lzi: {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
-    },
-    testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
     },
     }
     //ARCHIVE https://telegra.ph/APIs-08-12
